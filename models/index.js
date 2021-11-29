ed28dbcc2b4a925ed2430783ad426c8e3544bdea
Gustavo Corchog <gugacorchog@gmail.com>
2021-11-29 21:39:58 +1100

    update

--- models/index.js
+++ models/index.js
@@ -1,17 +1,34 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
-// Categories have many Products
+Product.belongsTo(Category,
+  {
+    foreignKey:'category_id'
+  });
 
-// Products belongToMany Tags (through ProductTag)
 
-// Tags belongToMany Products (through ProductTag)
+Category.hasMany(Product,
+  {
+    foreignKey:'category_id'
+  });
 
+
+Product.belongsToMany(Tag,
+  {
+    through:ProductTag,
+    foreignKey:'product_id'
+  });
+
+
+Tag.belongsToMany(Product,
+  {
+    through:ProductTag,
+    foreignKey:'tag_id'
+  });
+  
 module.exports = {
   Product,
   Category,
